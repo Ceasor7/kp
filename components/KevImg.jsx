5dc9b259baf0227dea12794669fc117ad3dce8b9
Ceasor7 <josegee0707@gmail.com>
2024-09-02 17:12:05 +0300

    first commit

---
+++ components/KevImg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const KevImg = () => {
+  return (
+    <div>KevImg</div>
+  )
+}
+
+export default KevImg
---
+++ components/Socials.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {
+  RiYoutubeFill,
+  RiLinkedinFill,
+  RiInstagramFill,
+  RiFacebookBoxFill,
+  RiTwitterXFill
+} from 'react-icons/ri'
+import Link from 'next/link'
+
+const icons = [
+  {path: '/',
+    name: <RiFacebookBoxFill/>
+  },
+  {path: '/',
+    name: <RiLinkedinFill/>
+  },
+  {path: '/',
+    name: <RiYoutubeFill/>
+  },
+  {path: '/',
+    name: <RiTwitterXFill/>
+  },
+  {path: '/',
+    name: <RiInstagramFill/>
+  },
+]
+
+const Socials = ({containerStyles, iconsStyles}) => {
+  return (
+    <div className={`${containerStyles}`}>
+      {icons.map((icon, index) => {
+        return (
+          <Link href={icon.path} key={index}>
+            <div className={`${iconsStyles}`}>{icon.name}</div>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Socials
